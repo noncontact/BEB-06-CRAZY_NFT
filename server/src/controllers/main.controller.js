2d9966278ee80c130006f064c4b364260b30c9ff
jm0717 <jm0717@hanmail.net>
2022-11-22 17:58:14 +0900

    main controller modify

--- server/src/controllers/main.controller.js
+++ server/src/controllers/main.controller.js
@@ -1,13 +1,9 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-const contract_proc = require("../process/contract.process");
+//const contract_proc = require("../process/contract.process");
 
 exports.main_get = async(req, res, next) => {
 
-    const result = await contract_proc.testFunction();
-    //res.status(200).send("Hello BEB-06-OPST");
-    res.status(200).send(result);
+    //const result = await contract_proc.testFunction();
+    //res.status(200).send(result);
+    res.status(200).send("Hello BEB-06-OPST");
+    
 }
