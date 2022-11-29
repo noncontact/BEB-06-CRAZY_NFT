884b870d58f14f916ee08486d20071d4b7d19652
EunSeokCho <shcho9027@naver.com>
2022-11-29 18:16:09 +0900

    클럽페이지 기능 및 nft deploy테스트기능

--- client/src/component/club/ClubMain/Articles.js
+++ client/src/component/club/ClubMain/Articles.js
@@ -1,16 +1,27 @@
 import {List,Button} from "antd";
 import { EditOutlined } from '@ant-design/icons';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clubContents } from "../../../api/club";
 import React,{ useState,useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
-
+import { useLocation, useNavigate } from "react-router-dom";
+/**User
+{id: , nickname: , profileurl:'}
+content,
+createdAt,
+id,
+img,
+title
+}
+ */
 const Articles =()=>{
     const [articles,setArticles]=useState([]);
+    const curLoca=useLocation();
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
     const {clubId,clubName,catagoryId,catagory}=useSelector((state) =>{
         return state.club;
     });
-    const navigate=useNavigate();
+    
 
     useEffect(() => {
 
@@ -18,11 +29,17 @@ const Articles =()=>{
         const contents=await clubContents(clubId,catagoryId);
         
         setArticles(contents.data.data);
+        console.log(contents.data.data);
         };
 
         fetchData();
     }, [catagoryId]);
-    
+
+    const selectArticle = (id)=>{
+        dispatch({type:"clubSlice/selectPost",payload:{post_id:id}});
+        navigate(curLoca.pathname+`/articledetail/${id}`);
+    };
+
     return (
         <List
         size="large"
@@ -34,7 +51,7 @@ const Articles =()=>{
             pageSize: 10,
           }}
         dataSource={articles}
-        renderItem={(item) => <List.Item style={{border:"1px solid gray"}}>{item}</List.Item>}
+        renderItem={(item) => <List.Item onClick={()=>selectArticle(item.id)} style={{border:"1px solid gray"}}>title{item.title}  nickname{item.User.nickname}</List.Item>}
         />
     );    
 };
